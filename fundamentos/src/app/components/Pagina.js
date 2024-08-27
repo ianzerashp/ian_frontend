13aa4b6b4cea041113c732452e67ab53773a4c7f
ianzerashp <ianbarros77@gmail.com>
2024-08-26 21:40:00 -0300

    Mexendo na página e colocando atividade 2

--- fundamentos/src/app/components/Pagina.js
+++ fundamentos/src/app/components/Pagina.js
@@ -4,15 +4,16 @@ export default function Pagina(props) {
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+        
+        <Navbar.Brand href="#home">Sistema Acadêmico</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link href="#home">Página 1</Nav.Link>
+            <Nav.Link href="#features">Página 2</Nav.Link>
+            <Nav.Link href="#pricing">Array</Nav.Link>
+            <Nav.Link href="#pricing">Carros</Nav.Link>
+        </Nav>
+    
+</Navbar>
 
             <div className="bg-secondary text-white text-center p-3">
                 <h1>{props.titulo}</h1>
